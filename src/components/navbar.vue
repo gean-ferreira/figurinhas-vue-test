73d94e44582960dff48756503c8af54ca690470b
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-24 11:55:55 -0300

    CHORE: Deixando a barra de navegacao reativa

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,8 +1,12 @@
 <template>
     <nav id="navbar">
         <ul>
+            <router-link to="/">
                 <li>Produtos</li>
+            </router-link>
+            <router-link to="/carrinho">
                 <li>Carrinho</li>
+            </router-link>
         </ul>
     </nav>
 </template>
