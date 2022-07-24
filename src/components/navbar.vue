af2fc8a5a38a2801ca39b6c3a3fa66b7013eae48
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-24 11:57:34 -0300

    STYLE: Estilizando o componente navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -10,3 +10,29 @@
         </ul>
     </nav>
 </template>
+
+<style scoped>
+@import "../assets/base.css";
+
+#navbar ul {
+    display: flex;
+    justify-content: flex-end;
+    padding: 1rem;
+    gap: 24px;
+}
+
+#navbar ul a {
+    text-decoration: none;
+    color: var(--neutral);
+}
+
+#navbar ul a:hover,
+#navbar ul a:active {
+    text-decoration: underline;
+}
+
+#navbar ul li {
+    list-style: none;
+    color: var(--neutral);
+}
+</style>
