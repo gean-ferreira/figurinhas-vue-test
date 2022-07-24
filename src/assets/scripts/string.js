4130127fd8812300d6aedab8fd312a6a63312a70
Gean Ferreira <gean_roberto@hotmail.com>
2022-07-24 13:39:48 -0300

    DOC: Documentando funcao

--- src/assets/scripts/string.js
+++ src/assets/scripts/string.js
@@ -1,4 +1,5 @@
 const stringManipulation = {
+    // Coloca em no plural a palavra/titulo de acordo com a quantidade
     pluralWord(quantity, word) {
         return parseInt(quantity) > 1 ? `${word}s` : word;
     },
